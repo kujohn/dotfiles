3289fa5f926af694f8640ef2f4f75cd46220a1e6
John Ku <hellojohnku@gmail.com>
2020-08-14 12:27:12 -0700

    wip

--- .lein/.lein/profiles.clj
+++
@@ -1,9 +0,0 @@
-{
- :user {
-        :plugins [[lein-exec "0.3.7"]]
-        }
- :repl {
-        :dependencies [[cljfmt "0.5.1"] [jonase/eastwood "0.2.1" :exclusions [org.clojure/clojure]]]
-        :plugins [[lein-cljfmt "0.6.7"]
-                  [jonase/eastwood "0.3.10"]
-                  [cider/cider-nrepl "0.24.0"]]}}
