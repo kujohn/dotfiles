fdc18868d4b025721a3f65e8c8eee90f089bb1a8
John Ku <john_ku@apple.com>
2024-10-10 09:56:23 -0700

    update for swift

--- quickstart.sh
+++ quickstart.sh
@@ -10,6 +10,7 @@ brew install z
 brew install lsd
 brew install efm-langserver
 brew install zoxide
+brew install swift-format
 
 brew install yazi ffmpegthumbnailer unar jq poppler fd ripgrep fzf zoxide
 brew tap homebrew/cask-fonts && brew install --cask font-symbols-only-nerd-font
