bbe60cc4a78f1ac17409c9fe836260397c4859dd
John Ku <john_ku@apple.com>
2025-02-27 08:51:45 -0800

    nvim with xcode

---
+++ .config/nvim/init.lua
@@ -0,0 +1,49 @@
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Setup lazy.nvim
+require("lazy").setup({
+  spec = {
+    {
+  "wojciech-kulik/xcodebuild.nvim",
+  dependencies = {
+    "nvim-telescope/telescope.nvim",
+    "MunifTanjim/nui.nvim",
+    "folke/snacks.nvim", -- (optional) to show previews
+    "nvim-tree/nvim-tree.lua", -- (optional) to manage project files
+    "stevearc/oil.nvim", -- (optional) to manage project files
+    "nvim-treesitter/nvim-treesitter", -- (optional) for Quick tests support (required Swift parser)
+  },
+  config = function()
+    require("xcodebuild").setup({
+        -- put some options here or leave it empty to use default settings
+    })
+  end,
+}
+  },
+  -- Configure any other settings here. See the documentation for more details.
+  -- colorscheme that will be used when installing plugins.
+  install = { colorscheme = { "habamax" } },
+  -- automatically check for plugin updates
+  checker = { enabled = true },
+})
