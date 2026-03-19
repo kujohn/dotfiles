5f22ed317524e6594124b5ede93c426fb1df94e8
John Ku <hellojohnku@gmail.com>
2026-03-19 13:16:57 -0700

    Update quickstart.sh

--- quickstart.sh
+++ quickstart.sh
@@ -8,6 +8,7 @@ brew install --cask ghostty
 brew install --cask wezterm
 brew install --cask cleanshot
 brew install --cask rectangle
+brew install starship
 brew install stow
 brew install ripgrep
 brew install the_silver_searcher
